7e2a7056d2a056e8923d2fd40e6177e2d1a3371a
Cristi <cristi21cristi2124@gmail.com>
2025-03-30 19:49:49 +0300

    Actualizare lamba warning

--- src/main/java/com/parcare/parcare_sistem/config/SecurityConfig.java
+++ src/main/java/com/parcare/parcare_sistem/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
 
+
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
